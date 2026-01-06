64c72e38c50c41b6bef611f17c2e8fa7ce5df436
Jeferson <themerior@hotmail.com>
2026-01-06 18:16:06 -0300

    feat(ui): finalizando o layout das tela no Windows Forms.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -37,85 +36,75 @@
             button1 = new Button();
             button2 = new Button();
             comboBox1 = new ComboBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
-            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(12, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(160, 160);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(220, 197);
+            label1.Location = new Point(31, 6);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(251, 15);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Selecione o processo para o ducking de áudio.";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(220, 226);
+            label2.Location = new Point(22, 53);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(239, 15);
             label2.TabIndex = 2;
-            label2.Text = "label2";
+            label2.Text = "Abaixar o volume do som do processo para:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(220, 255);
+            label3.Location = new Point(267, 53);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(25, 15);
             label3.TabIndex = 3;
-            label3.Text = "label3";
+            label3.Text = "000";
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(411, 255);
+            trackBar1.Location = new Point(31, 71);
+            trackBar1.Maximum = 100;
             trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(104, 45);
+            trackBar1.Size = new Size(250, 45);
             trackBar1.TabIndex = 4;
             // 
             // button1
             // 
-            button1.Location = new Point(629, 189);
+            button1.Location = new Point(78, 108);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 5;
-            button1.Text = "button1";
+            button1.Text = "Inciar";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(629, 222);
+            button2.Location = new Point(159, 108);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 6;
-            button2.Text = "button2";
+            button2.Text = "Parar";
             button2.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(220, 334);
+            comboBox1.Location = new Point(15, 24);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
+            comboBox1.Size = new Size(282, 23);
             comboBox1.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(312, 143);
             Controls.Add(comboBox1);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -123,18 +112,15 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Audio Ducking";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Label label1;
         private Label label2;
         private Label label3;
