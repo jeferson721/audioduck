ae035b227de0aca6baa05583c0d85c08013e0657
Jeferson <themerior@hotmail.com>
2026-02-01 22:44:33 -0300

    Adiciona classe FonteDoAudioDuck e novos namespaces
    Criado novo arquivo AudioDuck.cs e incluído o namespace ControleAudioDuck, com a nova classe FonteDoAudioDuck.

---
+++ AudioDuck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleAudioDuck
+{
+    public class FonteDoAudioDuck
+    {
+        public FonteDoAudioDuck()
+        {
+
+        }
+    }
+}
