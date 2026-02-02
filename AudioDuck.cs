c9fbc98b09d3bb2469c230d765364b0edaba710a
Jeferson <themerior@hotmail.com>
2026-02-01 22:50:13 -0300

    Suprime aviso IDE0130.

--- AudioDuck.cs
+++ AudioDuck.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
+#pragma warning disable IDE0130 
+/* ATENÇÃO esse "pragma warning disable IDE0130" suprimi a violação 
+ * da regra de estilo que usa a estrutura de pastas do projeto para 
+ * impor requisitos de nomenclatura de namespace. WARNING: O namespace
+ * não corresponde à estrutura da pasta (IDE0130). */
+
 namespace ControleAudioDuck
 {
     public class FonteDoAudioDuck
